8e28338c3328fe440fb3d58b18892549a24133e2
aurelioo29 <aureliolucio5@gmail.com>
2025-10-20 09:15:57 +0700

    add favicon for login pages

--- resources/views/auth/dokter/login.blade.php
+++ resources/views/auth/dokter/login.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link rel="icon" href="{{ asset('images/logo_rsgm.png') }}">
     <title>Login — Logbook Dokter</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
